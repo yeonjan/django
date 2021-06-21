e6ec56c0c305123c5fde8df9345e63b41fdc4417
yeonjin <60567697+yjin99@users.noreply.github.com>
2021-06-21 22:39:22 +0900

    Add files via upload

---
+++ my_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    "default" : {
+      "ENGINE": "django.db.backends.mysql",
+      "NAME": "web",
+      "USER": "root",
+      "PASSWORD": "wjd1025",
+      "HOST": "localhost",
+      "PORT": "3306"
+    }
+}
+SECRET_KEY = 'django-insecure-iiayg6jc6=ssx0^&^ar_fy#jhm3*5_prleu+d4c6sgu!de3&+&'
